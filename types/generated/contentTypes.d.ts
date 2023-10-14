7dfe834b386a2eea0ee8f15f3afe5b3eac453b86
nexzanavod <aanavod@gmail.com>
2023-10-14 23:27:30 +0530

    new changes

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -742,11 +742,7 @@ export interface ApiVoterVoter extends Schema.CollectionType {
   attributes: {
     Name: Attribute.String;
     NIC_Number: Attribute.String & Attribute.Unique;
-    Title: Attribute.Enumeration<
-      ['Madam', 'Mr', 'Mrs', 'Ms', 'Miss', 'Dr', 'Professor ']
-    >;
     Occupation: Attribute.String;
-    Gender: Attribute.Enumeration<['Male', 'Female', 'Other']>;
     Address: Attribute.String;
     Civil_Status: Attribute.String;
     NJP_Party_Member: Attribute.Boolean;
@@ -755,151 +751,19 @@ export interface ApiVoterVoter extends Schema.CollectionType {
     Mobile_Number_2: Attribute.String;
     WhatsApp_Number: Attribute.String;
     Facebook_Link: Attribute.String;
-    District: Attribute.Enumeration<['Colombo']>;
-    Seat: Attribute.Enumeration<
-      [
-        'ColomboNorth',
-        'ColomboCentral',
-        'Borella',
-        'ColomboEast',
-        'ColomboWest',
-        'Dehiwala',
-        'Ratmalana',
-        'Kollonnawa',
-        'Kotte',
-        'Kaduwela',
-        'Awissawella',
-        'Homagama',
-        'Maharagama',
-        'Kesbewa',
-        'Moratuwa'
-      ]
-    >;
-    Local_Authority: Attribute.Enumeration<
-      [
-        'Colombo Municipal Council',
-        'Dehiwala',
-        'MountLavinia',
-        'Municipal Council Dehiwala',
-        'MountLavinia Municipal Council',
-        'Kollonnawa Urban Council',
-        'Kotikawatta',
-        'Mulleriyawa Pradeshiyasabha',
-        'Sri Jayawardenepura Kotte Municipal Council',
-        'Kaduwela Municipal Council',
-        'Seethawakapura Urban Council',
-        'Seethawakapura Pradeshiyasabha',
-        'Homagama PradeshiyaSabha',
-        'Maharagama UrbanCouncil',
-        'Kesbewa UrbanCouncil',
-        'Boralesgamuwa Urban Council',
-        'Moratuwa Municipal Council'
-      ]
-    >;
-    Ward: Attribute.Enumeration<
-      [
-        'Sammanthranapura',
-        'Mattakkuliya',
-        'Modara',
-        'Mahawatta',
-        'AluthMawatha',
-        'Lunupokuna',
-        'KotahenaWest',
-        'KotahenaEast',
-        'Bloemendhal',
-        'Jintupitiya',
-        'Kochchikade',
-        'KochchikadeSouth',
-        'GrandpassNorth',
-        'Navagampura',
-        'MaligawattaEast',
-        'GrandpassSouth',
-        'Khettarama',
-        'NewBazaar',
-        'Masangasweediya',
-        'Fort',
-        'HultsdorfWest',
-        'HultsdorfEast',
-        'Keselwatta',
-        'Panchikawatta',
-        'MaligawattaWest',
-        'Maradana',
-        'Hunupitiya',
-        'Wekanda',
-        'Dematagoda',
-        'KuppiyawattaEast',
-        'Wanathamulla',
-        'BorellaNorth',
-        'KuppiyawattaWest',
-        'CinnamonGardens',
-        'BorellaSouth',
-        'Gothamipura',
-        'Narahenpita',
-        'Thimbirigasyaya'
-      ]
-    >;
-    GN_Division: Attribute.Enumeration<
-      [
-        'Sammanthranapura 005',
-        'Mattakkuliya 010',
-        'Modara 015',
-        'Madampitiya 020',
-        'Mahawatta 025',
-        'Aluthmawatha 030',
-        'Lunupokuna 035',
-        'Kotahena West 050',
-        'Kotahena East 045',
-        'Bloemendhal 040',
-        'Jinthupitiya 060',
-        'Kochchikade North 055',
-        'Kochchikade South 110',
-        'Grandpass North 080',
-        'Nawagampura 085',
-        'Maligawatta East 090',
-        'Grandpass South 075',
-        'Kettarama 095',
-        'New Bazaar 070'
-      ]
-    >;
-    Street_Village: Attribute.Enumeration<
-      [
-        'Sri Wickramapura',
-        'Sammanthranapura Road',
-        'Gemunu pura',
-        'Pichchamal watta',
-        'Gemunupura Road',
-        'Hadala Thotupla Rd',
-        'farm Rd',
-        'kadiranawath Housing   Scheme',
-        'Mudali john Rodrigo  Mw',
-        'Modara Street',
-        'S.D Fernando Road',
-        "St. Mery's Road",
-        'Pranshawatta Lane',
-        'Kelani Ganga Mill Road',
-        'Sri Kalyani gangarama Mawatha',
-        'Sri Wckrama Mawatha',
-        'Rawatta Lane',
-        'Malwatta Lane',
-        'Center Road',
-        'Saviya Lane',
-        'Fergussion Lane',
-        'Crow Island Road',
-        'Rodrigo place',
-        'Jubili Mawatha',
-        'Church Lane',
-        'Madampitiya Road',
-        'Bloemendhal Road',
-        'Mahawaththa Rd',
-        'Henamulla lane'
-      ]
-    >;
     District_Organizer: Attribute.String;
     Seat_Organizer: Attribute.String;
     Local_Authority_Organizer: Attribute.String;
     Ward_Organizer: Attribute.String;
     GN_Division_Organizer: Attribute.String;
     Street_Village_Organizer: Attribute.String;
+    Title: Attribute.String;
+    Gender: Attribute.String;
+    District: Attribute.Integer;
+    Local_Authority: Attribute.Integer;
+    Seat: Attribute.Integer;
+    GN_Division: Attribute.Integer;
+    Street_Village: Attribute.Integer;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
